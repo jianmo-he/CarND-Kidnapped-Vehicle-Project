7e9e6cefe895a4243e3981be80c9f2be252543a9
jianmo-he <jianmo.he@outlook.com>
2019-04-17 19:58:18 +0000

    compiled and working

--- src/particle_filter.h
+++ src/particle_filter.h
@@ -108,6 +108,9 @@ class ParticleFilter {
   std::string getAssociations(Particle best);
   std::string getSenseCoord(Particle best, std::string coord);
 
+  	std::string getSenseX(Particle best);
+	std::string getSenseY(Particle best);
+  
   // Set of current particles
   std::vector<Particle> particles;
 
